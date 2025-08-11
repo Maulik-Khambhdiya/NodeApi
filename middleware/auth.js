6f01361693d9420bec1319dc447bf387c9cdb7c1
Maulik-khambhdiya <maulikkhambhdiya444@gmail.com>
2025-08-11 17:44:50 +0530

    middleware

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const USER = require("../model/api");
+exports.authCheck = async (req, res, next) => {
+  // console.log("======");
+
+  try {
+    const token = req.headers.authorization;
+    // console.log(token);
+
+    if (!token) throw new Error("Attach token");
+
+    const tokenVerify = jwt.verify(token, "surat");
+
+    console.log(tokenVerify);
+
+    if (!tokenVerify) throw new Error("Invalid token");
+
+    const userVerify = await USER.findById(tokenVerify.id);
+
+    if (!userVerify) throw new Error("Invalid User");
+
+    next();
+  } catch (error) {
+    res.status(404).json({
+      status: "Fail",
+      message: error.message,
+    });
+  }
+};
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 const express= require('express')
 const apiRouter= express.Router()
 const multer=require('multer')  // for input image  
+const AM= require("../middleware/auth")//  for verify token and user
 
 
-//---------------------------------------------------------
+//---------------multer code-------------------------------
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -23,9 +24,9 @@ const upload = multer({ storage: storage })
 let AC = require('../controller/user')
 
 apiRouter.post('/',upload.single('profile'),AC.createData)
-apiRouter.get('/',AC.viewData)
-apiRouter.delete('/:id',AC.deleteData)
-apiRouter.patch('/:id',AC.editData)
+apiRouter.get('/',AM.autCheck,AC.viewData)                                 
+apiRouter.delete('/:id',AM.autCheck,AC.deleteData)
+apiRouter.patch('/:id',AM.autCheck,AC.editData)
 apiRouter.post('/login',AC.loginUser)  // login
  
 module.exports = apiRouter
